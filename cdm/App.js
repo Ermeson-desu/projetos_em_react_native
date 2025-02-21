43131a87b291d6e025fef3c066d1fdd6522ff790
Ermeson-desu <ermesondesu@gmail.com>
2025-02-21 11:36:12 -0300

    Ermeson 21/02/2025 11:35
    Tentei fazer o consumo de API, mais uma tentativa falha.

--- cdm/App.js
+++ cdm/App.js
@@ -2,19 +2,22 @@ import { Routes } from './src/routes';
 import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
-import {useFonts,JosefinSans_400Regular} from '@expo-google-fonts/josefin-sans'
-import {JotiOne_400Regular} from '@expo-google-fonts/joti-one'
-import {theme} from './src/styles'
-import Api from '@/src/services/Api'
+import {useFonts,JosefinSans_400Regular} from '@expo-google-fonts/josefin-sans';
+import {JotiOne_400Regular} from '@expo-google-fonts/joti-one';
+import {theme} from './src/styles';
+import {Api} from './src/services/Api';
 import { useState } from 'react'; 
+
+
 
 
 export default function App() {
   const [title, setTitle] = useState("")
 
+  //Função para buscar o mangá pelo nome
   async function buscarManga(){
     if(title.trim() === "") {
-      Alert.alert("Nome Inválido", "Por favor, digite um nome válido");
+      Alert.alert("Nome Inválido , Por favor, digite um nome válido");
       return
     }
       
@@ -22,7 +25,7 @@ export default function App() {
       const response = await Api.get(`/${title}`)
       setTitle(response.title)
     } catch (error) {
-      Alert.alert("Erro", "Não foi possível buscar os dados. Verifique o título do mangá buscado e tente novamente.")
+      Alert.alert("Erro: Não foi possível buscar os dados. Verifique o título do mangá buscado e tente novamente.")
     }
     }
     
--- cdm/src/services/Api.js
+++ cdm/src/services/Api.js
@@ -2,29 +2,8 @@
 import axios from 'axios';
 
 // URL base da API
-const API_URL = 'https://mangahook-api.vercel.app/mangalist';
+export const API = axios.create({
 
-/**
- * Função para obter a lista de mangás da API.
- * Realiza uma requisição GET para a API e retorna os dados de mangás.
- * @param {number} page - Página da lista de mangás (padrão é 1).
- * @param {string} category - Categoria dos mangás (padrão é "all").
- * @param {string} type - Tipo de mangá (padrão é "newest").
- * @param {string} state - Estado do mangá (padrão é "Ongoing").
- * @returns {Object} - Dados dos mangás e metadados da API.
- */
-export const getMangaList = async (
-    page=1, 
-    category = "all",
-    type="newest",
-    state = "Ongoing") => {
+    baseURL: 'https://mangahook-api.vercel.app/mangalist'
 
-        try{
-            const response = await axios.get(API_URL ,
-                {params: {page, category, type, state}})
-                return(response.data) // Retorna os dados recebidos
-        }catch (error){
-            console.error("Erro ao buscar o Mangá:", error)
-            return {mangaList:[], metaData:{}}
-        }
-    }
+});
