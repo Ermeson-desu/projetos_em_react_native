08aca038fa951d08b81dbe0a679824245fb43fd4
Ermeson-desu <ermesondesu@gmail.com>
2025-02-24 23:08:50 -0300

    24/02/2025 23:08
    Ajeitei o bug do scroll da tela de Home

--- cdm/src/components/Organisms/HomeList/index.js
+++ cdm/src/components/Organisms/HomeList/index.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const HomeList = styled.View`
+export const HomeList = styled.ScrollView`
     flex: 1;
     width: 100%;
     margin-bottom:${({theme})=>theme.metrics.px(70)}px;
